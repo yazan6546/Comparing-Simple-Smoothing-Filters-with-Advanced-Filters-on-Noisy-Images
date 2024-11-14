f04fb292b0a2995df9fde790dc098f87ceffb4db
yazan AbuAloun <61243898+yazan6546@users.noreply.github.com>
2024-11-14 08:41:50 +0000

    removed tensorflow_datasets and converted to reading images from local machine.

---
+++ utilities.py
@@ -0,0 +1,32 @@
+import image_processing as ip
+import pandas as pd
+
+def create_dataframe_image(original_image):
+  # Define noise parameters
+
+
+  gaussian_std = [5, 25, 75]
+  salt_pepper_probs  = [0.05, 0.25, 0.5]
+
+  # Create lists for DataFrame
+  index = []
+  noisy_images = []
+
+  index.append('no_noise')
+  noisy_images.append(original_image)
+
+  # Apply Gaussian noise with different variances
+  for sigma in gaussian_std:
+      noisy_img = ip.add_gaussian_noise(original_image, sigma=sigma)
+      index.append(f'Gaussian_Noise_sigma_{sigma}')
+      noisy_images.append(noisy_img)
+
+  # Apply Salt-and-Pepper noise with different probabilities
+  for prob in salt_pepper_probs:
+      noisy_img = ip.add_salt_and_pepper_noise(original_image, prob=prob)
+      index.append(f'Salt_and_Pepper_prob_{prob}')
+      noisy_images.append(noisy_img)
+
+  # Create DataFrame
+  df = pd.DataFrame({'Noise_Type': index, 'Image': noisy_images}).set_index('Noise_Type')
+  return df
