78307ba343ac702de1b3318ea61a574e0a011626
yazan AbuAloun <61243898+yazan6546@users.noreply.github.com>
2024-11-15 19:19:48 +0000

    added introduction

--- visualization.py
+++ visualization.py
@@ -1,9 +1,5 @@
 import matplotlib.pyplot as plt
-import cv2
 import math
-
-
-
 def plot_metric_vs_kernel(metric_dict_outer, ylabel, noise_levels, filter_types, kernel_sizes, number):
     num_noise_types = len(metric_dict_outer)
     num_noise_levels = len(noise_levels)
